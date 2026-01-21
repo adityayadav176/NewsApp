abef929218bfb1dd7cb5410e7a7c1a0908472fce
adityayadav176 <ay6598aditya@gmail.com>
2026-01-21 23:56:08 +0530

    add  spinner components

---
+++ src/components/spinner.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class spinner extends Component {
+  render() {
+    return (
+      <div>
+        <span class="loader"></span>
+      </div>
+    )
+  }
+}
